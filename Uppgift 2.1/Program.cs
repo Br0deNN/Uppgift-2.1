14871aee5897524279766fde599267232eba81c2
7R1XY <theo.broden@gmail.com>
2023-09-15 12:27:00 +0200

    Add project files.

---
+++ Uppgift 2.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Uppgift_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilken är din favorit bok?");
+            string Bok = Console.ReadLine();
+            Console.WriteLine("Din favorit bok är " + Bok);
+            Console.ReadKey();
+        }
+    }
+}
